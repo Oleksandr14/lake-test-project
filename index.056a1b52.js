862c135a133f34bca40dabf1987627d561108d3f
Oleksandr14 <96308880+Oleksandr14@users.noreply.github.com>
2023-04-02 12:46:09 +0000

    Deploying to gh-pages from @ Oleksandr14/lake-test-project@78b9fbe479b803f445fbb64d7b986933f0f37d17 🚀

---
+++ index.056a1b52.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector(".scroll-up");window.addEventListener("scroll",(function(){(window.pageYOffset||document.documentElement.scrollTop)>100?o.classList.add("scroll-up--active"):o.classList.remove("scroll-up--active")})),o.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}))}();
+//# sourceMappingURL=index.056a1b52.js.map
---
+++ index.459566c2.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".scroll-up");window.addEventListener("scroll",(()=>{(window.pageYOffset||document.documentElement.scrollTop)>100?e.classList.add("scroll-up--active"):e.classList.remove("scroll-up--active")})),e.addEventListener("click",(()=>{window.scrollTo({top:0,behavior:"smooth"})}));
+//# sourceMappingURL=index.459566c2.js.map
