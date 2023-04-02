78b9fbe479b803f445fbb64d7b986933f0f37d17
Oleksandr Palamarchuk <palamarchuk.oa93@gmail.com>
2023-04-02 15:45:32 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+const offset = 100;
+const scrollUp = document.querySelector('.scroll-up');
 
+const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+window.addEventListener('scroll', () => {
+  if (getTop() > offset) {
+    scrollUp.classList.add('scroll-up--active');
+  } else {
+    scrollUp.classList.remove('scroll-up--active');
+  }
+});
+
+scrollUp.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
